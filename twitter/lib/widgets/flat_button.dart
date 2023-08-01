d5a1ff3920ba2573dd80e4365df0b984825d9b78
Rémi Marçais <remi.marcais87@gmail.com>
2023-08-01 12:21:50 +0200

    Create the CustomFlatButton widget

---
+++ twitter/lib/widgets/flat_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomFlatButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+
+  const CustomFlatButton({super.key, required this.label, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      child: Text(
+        label,
+        style: const TextStyle(
+          fontFamily: "Raleway",
+          fontSize: 30,
+          height: 1,
+        ),
+      ),
+    );
+  }
+}
