ed7431743ce3929e197a76aaac1f0b2e95e015e3
Rémi Marçais <remi.marcais87@gmail.com>
2023-08-06 22:33:07 +0200

    Edit Notifications and Chats pages

--- twitter/lib/screens/chats_screen.dart
+++ twitter/lib/screens/chats_screen.dart
@@ -10,7 +10,60 @@ class ChatsScreen extends StatefulWidget {
 class _ChatsScreen extends State<ChatsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color.fromRGBO(250, 250, 250, 255),
+        title: Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Row(
+            children: [
+              Container(
+                width: 40,
+                height: 40,
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            "https://media.licdn.com/dms/image/D4E03AQFax6fciha3yg/profile-displayphoto-shrink_800_800/0/1675347507006?e=2147483647&v=beta&t=HWpYg68Lvvbh7UP6VeaFpRSrnX_xXHszsjwPGqIfFZ0")
+                    )
+                )
+              ),
+              const SizedBox(width: 20),
+              const Expanded(
+                child: Text(
+                  "Chat",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.black87
+                  ),
+                ),
+              ),
+              const SizedBox(width: 20),
+              IconButton(
+                icon: const Icon(
+                  Icons.settings_outlined,
+                  color: Colors.blue,
+                  size: 30,
+                ),
+                onPressed: () {},
+              )
+            ],
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          "No Chats Yet",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+      ),
     );
   }
 }
